b13ed57930e2deb41d61072a72edb51420f27c78
DolphQ <dolphqin@gmail.com>
2022-04-10 09:22:02 -0400

    Update Package_Verify filename

---
+++ Package_Verify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function PackageVerify(){
+	PackageVerifyInfo=$(dpkg -s $1 2> /dev/null)
+	PackageStatus=$(echo "$PackageVerifyInfo" | grep Status | awk -F: '{print $2}' |sed 's/^[\ ]//g')
+	if [[ $PackageStatus != 'install ok installed' ]];then
+		sudo apt-get install $1 > /dev/null
+		echo "Installed Package $1 "
+	fi
+}
+PackageVerify

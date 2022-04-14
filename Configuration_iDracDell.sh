6e8da31bc56035d53085195ced34917b9d9f37e7
DolphQ <dolphqin@gmail.com>
2022-04-14 09:01:56 -0400

    delete a mistake

--- Configuration_iDracDell.sh
+++ Configuration_iDracDell.sh
@@ -17,7 +17,7 @@ function ConfigurationiDracDell(){
 		NTPServer1=10.117.0.1
 		NTPServer2=10.111.0.1
 	fi
-	sudo sshpass -p calvin ssh -o StrictHostKeyChecking=no root@$iDracHostname > /dev/null 2>&1 << iDracConfiguratoin
+	sudo sshpass -p calvin ssh -o StrictHostKeyChecking=no root@$iDracHostname > iDracSysInfomation 2>&1 << iDracConfiguratoin
 racadm set idrac.users.3.username vmware
 racadm set idrac.users.3.password VMware1!
 racadm set idrac.users.3.enable 1
@@ -36,6 +36,9 @@ racadm set iDRAC.Nic.DNSDomainName eng.vmware.com
 iDracConfiguratoin
 }
 
+function ConfigurationVerify(){
+	return
+}
 
 function GetInfoDell(){
 # Get server's info from Dell iDrac
